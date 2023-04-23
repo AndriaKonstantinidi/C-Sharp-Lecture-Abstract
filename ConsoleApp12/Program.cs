52a86fe73d185ba1163a9c6cc4932396683d7d29
AndriaKonstantinidi <a97933199@gmail.com>
2023-04-23 19:36:04 +0400

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,73 @@
+﻿//public abstract class Animal
+//{
+//    public int a { get; set; }
+//    public abstract void MakeSound();
+//}
+
+//public class dog : Animal
+//{
+//    public override void MakeSound()
+//    {
+//        a = 5;
+//        Console.WriteLine("dogo");
+//    }
+//}
+
+//public class cat : Animal
+//{
+//    public override void MakeSound()
+//    {
+//        a = 6;
+//        Console.WriteLine("miau");
+//    }
+//}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A Jump = new A();
+        Jump.AbilitiesList();
+
+        B Sprint = new B();
+        Sprint.AbilitiesList();
+
+        C Fly = new C();
+        Fly.AbilitiesList();
+    }
+}
+
+public abstract class ABC
+{
+    public int a { get; set; }
+    public abstract void AbilitiesList();
+}
+
+public class A : ABC
+{
+    
+    public override void AbilitiesList()
+    {
+        a = 1;
+        Console.WriteLine(a+" "+"Can jump");
+    }
+}
+
+public class B : ABC
+{
+    public override void AbilitiesList()
+    {
+        a = 2;
+        Console.WriteLine(a+" "+"Can Sprint");
+    }
+}
+
+public class C : ABC
+{
+    public override void AbilitiesList()
+    {
+        a = 3;
+        Console.WriteLine(a+" "+"Can Fly");
+    }
+}
